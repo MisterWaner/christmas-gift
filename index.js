b9fc31fa90189f324df2668ec4d9c8f377c1029a
Erwan Soulas <erwan.soulas@gmail.com>
2023-12-24 15:31:50 +0100

    add comments

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const partOne = document.querySelector(".part-one");
 const partTwo = document.querySelector(".part-two");
 const chevron = document.querySelector(".chevron");
 
+/* Scroll to main container */
+function scrollToMain() {
+    arrow.addEventListener("click", () => {
+        mainContainer.scrollIntoView({ behavior: "smooth" });
+    });
 
+    setTimeout(() => {
+        partOne.classList.add("show-text");
+    }, 1500);
 
+    setTimeout(() => {
+        partTwo.classList.add("show-text");
+    }, 2500);
+}
+
+/* Switch to curtain */
+function switchToCurtain() {
+    chevron.addEventListener("click", () => {
+        window.location.href = "./curtain.html";
+    });
+}
+
+/* Launch song when DOM is loaded */
 document.addEventListener("DOMContentLoaded", () => {
 
     const isAudioPlaying = sessionStorage.getItem("isAudioPlaying");
@@ -22,23 +43,3 @@ document.addEventListener("DOMContentLoaded", () => {
         christmasSong.pause();
     })
 })
-
-function scrollToMain() {
-    arrow.addEventListener("click", () => {
-        mainContainer.scrollIntoView({ behavior: "smooth" });
-    });
-
-    setTimeout(() => {
-        partOne.classList.add("show-text");
-    }, 1500);
-
-    setTimeout(() => {
-        partTwo.classList.add("show-text");
-    }, 2500);
-}
-
-function scrollToCurtain() {
-    chevron.addEventListener("click", () => {
-        window.location.href = "./curtain.html";
-    });
-}
