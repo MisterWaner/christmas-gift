0c69d8ceec7ac6e97be6a90cdd98b8423aa512fd
Erwan Soulas <erwan.soulas@gmail.com>
2023-12-24 15:25:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const christmasSong = document.querySelector("#christmas-song");
+const arrow = document.querySelector(".arrow");
+const mainContainer = document.querySelector(".main-container");
+const partOne = document.querySelector(".part-one");
+const partTwo = document.querySelector(".part-two");
+const chevron = document.querySelector(".chevron");
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const isAudioPlaying = sessionStorage.getItem("isAudioPlaying");
+
+    if (isAudioPlaying) {
+        christmasSong.play();
+    } else {
+        christmasSong.play();
+        sessionStorage.setItem("isAudioPlaying", true);
+    }
+
+    window.addEventListener("beforeunload", () => {
+        christmasSong.pause();
+    })
+})
+
+function scrollToMain() {
+    arrow.addEventListener("click", () => {
+        mainContainer.scrollIntoView({ behavior: "smooth" });
+    });
+
+    setTimeout(() => {
+        partOne.classList.add("show-text");
+    }, 1500);
+
+    setTimeout(() => {
+        partTwo.classList.add("show-text");
+    }, 2500);
+}
+
+function scrollToCurtain() {
+    chevron.addEventListener("click", () => {
+        window.location.href = "./curtain.html";
+    });
+}
